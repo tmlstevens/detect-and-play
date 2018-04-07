cf572784601434580fb7021421f840428d3c384c
DESKTOP-H4FQR55\Hasan The Must <hmust92@gmail.com>
2018-04-07 11:42:17 -0500

    removed face analysis (unneeded)

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,25 +7,21 @@ $(document).ready(function(){
 
 // DETECT
     var imagePath;
-    var imageFile = "image_file=" + imagePath
+    var imageFile = "http://img.wennermedia.com/social/dwayne-the-rock-johnson-for-president-094088ba-7415-4b23-8a9a-0150b55749ec.jpg";
 
-    var detectURL =  "https://api-us.faceplusplus.com/facepp/v3/detect?api_key=N2r5hDIKEfpq8-C8_4R9FsHZ2IeohyU-&api_secret=bEnlP7JnI8FUw0q4m7SGXrqudZXeeBOP&" + imageFile + "&return_landmark=1&return_attributes=gender,age,smiling";
+    var detectURL =  "https://api-us.faceplusplus.com/facepp/v3/detect?api_key=N2r5hDIKEfpq8-C8_4R9FsHZ2IeohyU-&api_secret=bEnlP7JnI8FUw0q4m7SGXrqudZXeeBOP&image_url=" + imageFile + "&return_landmark=1&return_attributes=emotion";
     
     $.ajax({
         url: detectURL,
         method: "POST"
     }).then(function(response) {
         // console.log(response.faces[0].face_token);
-        var faceToken = response.faces[0].face_token;
-    });
+        
+        console.log(response);
+    })
+});
 
         
-// ANALYZE
-    var analyzeURL = "https://api-us.faceplusplus.com/facepp/v3/face/analyze?api_key=N2r5hDIKEfpq8-C8_4R9FsHZ2IeohyU-&api_secret=bEnlP7JnI8FUw0q4m7SGXrqudZXeeBOP&return_landmark=1&face_tokens=" + faceToken;
 
-    $.ajax({
-        url: analyzeURL,
-        method: "POST"
-    }).then(console.log)
 
-})
+
